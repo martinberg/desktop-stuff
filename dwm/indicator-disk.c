10bcf364ed1412b5526c789afdf45223768703af
h4xxel <isaksson.axel@telia.com>
2015-09-29 12:57:32 +0200

    dynamically show disks in the system

--- dwm/indicator-disk.c
+++ dwm/indicator-disk.c
@@ -1,14 +1,19 @@
 #include <sys/statvfs.h>
+#include <mntent.h>
 
 #include "dwm.h"
 #include "indicator.h"
 
 #define MENU_WIDTH 200
 
-static char *discs[]={
-	"/",
-	"/media/data",
+typedef struct MountList MountList;
+struct MountList {
+	char *dir;
+	char usage[40];
+	MountList *next;
 };
+
+static MountList *mnt = NULL;
 
 static struct {
 	Window window;
@@ -17,12 +22,59 @@ static struct {
 	int selected;
 } menu={0};
 
+static int get_mounts(MountList **mnt_list) {
+	FILE *f;
+	struct mntent *m;
+	MountList *tmp;
+	int i = 0;
+	
+	if(!mnt_list)
+		return 0;
+	
+	f = setmntent(_PATH_MOUNTED, "r");
+	
+	while((m = getmntent(f))) {
+		if(strstr(m->mnt_opts, "bind"))
+			continue;
+		if(m->mnt_fsname[0] != '/')
+			continue;
+		
+		tmp = malloc(sizeof(MountList));
+		tmp->dir = strdup(m->mnt_dir);
+		tmp->next = *mnt_list;
+		*mnt_list = tmp;
+		mnt_list = &(*mnt_list)->next;
+		i++;
+	}
+	
+	endmntent(f);
+	return i;
+}
+
+static void free_mounts(MountList **mnt_list) {
+	MountList *tmp;
+	
+	if(!mnt_list)
+		return;
+	
+	while(*mnt_list) {
+		tmp = (*mnt_list)->next;
+		free((*mnt_list)->dir);
+		free(*mnt_list);
+		*mnt_list = tmp;
+	}
+}
+
 static void menu_open(Indicator *indicator) {
+	int mounts;
+	if((mounts = get_mounts(&mnt)) <= 0)
+		return;
+	
 	menu.selected=-1;
 	menu.x=selmon->mx+indicator->x-MENU_WIDTH+indicator->width;
 	menu.y=bh;
 	menu.w=MENU_WIDTH;
-	menu.h=bh*(sizeof(discs)/sizeof(char *));
+	menu.h=bh*mounts;
 	menu.window=XCreateSimpleWindow(dpy, root, 
 		menu.x, menu.y, menu.w, menu.h,
 		1, dc.sel[ColBorder], dc.norm[ColBG]
@@ -31,12 +83,16 @@ static void menu_open(Indicator *indicator) {
 	XSelectInput(dpy, menu.window, ExposureMask|ButtonPressMask|PointerMotionMask);
 	XDefineCursor(dpy, menu.window, cursor[CurNormal]);
 	XMapRaised(dpy, menu.window);
+	indicator->active = 1;
 }
 
-static void menu_close() {
+static void menu_close(Indicator *indicator) {
+	free_mounts(&mnt);
+	
 	XFreeGC(dpy, menu.gc);
 	XUnmapWindow(dpy, menu.window);
 	XDestroyWindow(dpy, menu.window);
+	indicator->active = 0;
 }
 int indicator_disk_init(Indicator *indicator) {
 	return 0;
@@ -59,16 +115,28 @@ void indicator_disk_update(Indicator *indicator) {
 
 void indicator_disk_expose(Indicator *indicator, Window window) {
 	int i;
-	char buf[256];
+	unsigned long usage;
 	struct statvfs sbuf;
+	MountList *list;
 	
 	if(window!=menu.window)
 		return;
 	
-	for(i=0; i<(sizeof(discs)/sizeof(char *)); i++) {
-		if(statvfs(discs[i], &sbuf)>=0) {
-			sprintf(buf, " %s %lu%% ", discs[i], 100*(sbuf.f_blocks-sbuf.f_bavail)/sbuf.f_blocks);
-			indicator_draw_text(menu.window, menu.gc, 0, i*bh, MENU_WIDTH, bh, dc.norm, buf, False);
+	for(list = mnt, i = 0; list; list = list->next, i++) {
+		if(statvfs(list->dir, &sbuf) >= 0) {
+			usage = 100*(sbuf.f_blocks - sbuf.f_bavail)/sbuf.f_blocks;
+			sprintf(list->usage, " %lu%% ", usage);
+			
+			if(usage >= 95)
+				sprintf(list->usage, "<span foreground=\"red\">%lu%%</span>", usage);
+			else
+				sprintf(list->usage, "%lu%%", usage);
+			
+			indicator_draw_text(menu.window, menu.gc, 0, i*bh, MENU_WIDTH - 40, bh, dc.norm, list->dir, False);
+			indicator_draw_text(menu.window, menu.gc, MENU_WIDTH - 40, i*bh, 40, bh, dc.norm, list->usage, True);
+		} else {
+			indicator_draw_text(menu.window, menu.gc, 0, i*bh, MENU_WIDTH - 40, bh, dc.norm, list->dir, False);
+			indicator_draw_text(menu.window, menu.gc, MENU_WIDTH - 40, i*bh, 40, bh, dc.norm, "--", False);
 		}
 	}
 }
@@ -88,10 +156,10 @@ void indicator_disk_mouse(Indicator *indicator, XButtonPressedEvent *ev) {
 	switch(ev->button) {
 		case Button1:
 		case Button3:
-			if((indicator->active=!indicator->active))
-				menu_open(indicator);
+			if(indicator->active)
+				menu_close(indicator);
 			else
-				menu_close();
+				menu_open(indicator);
 			return;
 		case Button4:
 			break;
